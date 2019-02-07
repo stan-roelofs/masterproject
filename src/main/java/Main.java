c7852ead21e98197af4278600c86c3c4491be549
Stan Roelofs <s.roelofs@student.tue.nl>
2019-02-07 13:01:40 +0100

    Simple file reading implemented

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 import org.apache.commons.cli.*;
-import java.io.File;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,14 +15,31 @@ public class Main {
 
         try {
             // parse the command line arguments
-            CommandLine line = parser.parse( options, args );
+            CommandLine commandLine = parser.parse( options, args );
 
-            if (line.hasOption("f")) {
-                File input = new File(line.getOptionValue("f"));
+            if (commandLine.hasOption("i")) {
+                String fileName = commandLine.getOptionValue("i");
+
+                // Read file
+                try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+                    StringBuilder sb = new StringBuilder();
+                    String line = br.readLine();
+
+                    while (line != null) {
+                        sb.append(line);
+                        sb.append(System.lineSeparator());
+                        line = br.readLine();
+                    }
+                    String everything = sb.toString();
+
+                    System.out.println(everything);
+                }
             }
         }
         catch(ParseException exp) {
             System.err.println( "Parsing failed.  Reason: " + exp.getMessage() );
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }

3bb1374c6f91e31fe95f69125bb11d9821ec4d6d
Stan Roelofs <s.roelofs@student.tue.nl>
2019-02-28 16:09:39 +0100

    Implemented a basic equation parser

--- src/main/java/InputParser.java
+++ src/main/java/InputParser.java
@@ -4,17 +4,42 @@ class InputParser {
     static void parse(Collection<String> input) {
 
         for (String line : input) {
-            System.out.println(line);
+            if (line.contains("=")) {
+                parseEquation(line);
+            }
         }
     }
 
-    private Equation parseEquation(String line) {
+    private static Equation parseEquation(String line) throws IllegalArgumentException {
+        if (line == null) {
+            throw new IllegalArgumentException("line must not be null");
+        }
+        if (!line.contains("=")) {
+            throw new IllegalArgumentException("line does not contain '=', therefore is not an equation");
+        }
 
-        return null; //TODO
+        String[] split = line.split("=");
+        if (split.length > 2) {
+            throw new IllegalArgumentException("Equation contains more than one '='");
+        }
+
+        String leftString = split[0];
+        String rightString = split[1];
+
+        if (leftString.isEmpty() || rightString.isEmpty()) {
+            throw new IllegalArgumentException("Left or right cannot be empty");
+        }
+
+        Term left = parseTerm(leftString);
+        Term right = parseTerm(rightString);
+
+        return new Equation(left, right);
     }
 
-    private Term parseTerm(String line) {
-
+    private static Term parseTerm(String line) {
+        if (line == null) {
+            throw new IllegalArgumentException("line must not be null");
+        }
         return null; //TODO
     }
 }

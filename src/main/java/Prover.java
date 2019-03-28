5ecfd61f5a9a25ba670cf26def609b3acf9bce60
Stan Roelofs <s.roelofs@student.tue.nl>
2019-03-28 14:21:51 +0100

    Prover refactoring and fixed printing of steps

--- src/main/java/Prover.java
+++ src/main/java/Prover.java
@@ -11,7 +11,7 @@ import java.util.*;
 public class Prover {
 
     public static void induction(EquationSystem system, BufferedWriter outputWriter) throws IOException {
-        Equation goal = system.goal;
+        Equation goal = system.getGoal();
         outputWriter.write("Goal: " + goal.toString());
         outputWriter.newLine();
 
@@ -28,7 +28,7 @@ public class Prover {
             outputWriter.newLine();
 
             // For each function in C
-            for (Function function : system.C) {
+            for (Function function : system.getC()) {
 
                 List<Term> newConstants = new ArrayList<>();
                 List<Equation> hypotheses = new ArrayList<>();
@@ -77,117 +77,145 @@ public class Prover {
                 leftTerms.add(left);
                 rightTerms.add(right);
 
-                Set<Equation> allEquations = new HashSet<>(system.equations);
+                Set<Equation> allEquations = new HashSet<>(system.getEquations());
                 allEquations.addAll(hypotheses);
 
-                Map<Term, Term> steps = new HashMap<>();
+                Map<Term, Term> leftSteps = new HashMap<>();
+                Map<Term, Term> rightSteps = new HashMap<>();
 
                 while (checkConvergence(leftTerms, rightTerms) == null) {
-                    Set<Term> toAdd = new HashSet<>();
-
-                    for (Term term : leftTerms) {
-                        for (Equation eq : allEquations) {
-                            for (Term term2 : term.getAllSubTerms()) {
-                                // TODO: Right
-                                Map<Variable, Term> sub = eq.getLeft().getSubstitution(term2, new HashMap<>());
-
-                                if (sub != null) {
-                                    Term newt = eq.getRight();
-                                    for (Map.Entry<Variable, Term> entry : sub.entrySet()) {
-                                        newt = newt.substitute(entry.getKey(), entry.getValue());
-                                    }
-
-                                    toAdd.add(term.substitute(term2, newt));
-                                    if (!steps.containsKey(term.substitute(term2, newt))) {
-                                        steps.put(term.substitute(term2, newt), term);
-                                    }
-                                }
-
-                                // TODO: Right
-                                Map<Variable, Term> sub2 = eq.getRight().getSubstitution(term2, new HashMap<>());
-
-                                if (sub2 != null) {
-                                    Term newt = eq.getLeft();
-                                    for (Map.Entry<Variable, Term> entry : sub2.entrySet()) {
-                                        newt = newt.substitute(entry.getKey(), entry.getValue());
-                                    }
-
-                                    toAdd.add(term.substitute(term2, newt));
-                                    if (!steps.containsKey(term.substitute(term2, newt))) {
-                                        steps.put(term.substitute(term2, newt), term);
-                                    }
-                                }
-                            }
-                        }
-                    }
-
-                    leftTerms.addAll(toAdd);
-                    toAdd.clear();
-
-                    // tODO: steps right
-                    for (Term term : rightTerms) {
-                        for (Equation eq : allEquations) {
-                            for (Term term2 : term.getAllSubTerms()) {
-                                // TODO: Right
-                                Map<Variable, Term> sub = eq.getLeft().getSubstitution(term2, new HashMap<>());
-
-                                if (sub != null) {
-                                    Term newt = eq.getRight();
-                                    for (Map.Entry<Variable, Term> entry : sub.entrySet()) {
-                                        newt = newt.substitute(entry.getKey(), entry.getValue());
-                                    }
-
-                                    toAdd.add(term.substitute(term2, newt));
-                                    if (!steps.containsKey(term.substitute(term2, newt))) {
-                                        steps.put(term.substitute(term2, newt), term);
-                                    }
-                                }
-
-                                // TODO: Right
-                                Map<Variable, Term> sub2 = eq.getRight().getSubstitution(term2, new HashMap<>());
-
-                                if (sub2 != null) {
-                                    Term newt = eq.getLeft();
-                                    for (Map.Entry<Variable, Term> entry : sub2.entrySet()) {
-                                        newt = newt.substitute(entry.getKey(), entry.getValue());
-                                    }
-
-                                    toAdd.add(term.substitute(term2, newt));
-                                    if (!steps.containsKey(term.substitute(term2, newt))) {
-                                        steps.put(term.substitute(term2, newt), term);
-                                    }
-                                }
-                            }
-                        }
-                    }
-
-                    rightTerms.addAll(toAdd);
+                    leftTerms.addAll(rewriteAll(leftTerms, allEquations, leftSteps));
+                    rightTerms.addAll(rewriteAll(rightTerms, allEquations, rightSteps));
                 }
 
                 Term convergence = checkConvergence(leftTerms, rightTerms);
-                List<Term> revSequence = new ArrayList<>();
-
-                // Trace back the steps until we reach the start term
-                while (!(convergence.equals(left))) {
-                    revSequence.add(convergence);
-                    convergence = steps.get(convergence);
-                }
-                revSequence.add(convergence);
-
-                StringBuilder stepsString = new StringBuilder();
-                stepsString.append("Conversion: ");
-                for (int i = revSequence.size() - 1; i >= 0; i--) {
-                    stepsString.append(revSequence.get(i));
-
-                    if (i > 0) {
-                        stepsString.append(" -> ");
-                    }
+                if (convergence == null) {
+                    Logger.w("Convergence null");
+                    return;
                 }
 
-                outputWriter.write(stepsString.toString());
+                String conversion = getConversionString(left, right, convergence, leftSteps, rightSteps);
+                outputWriter.write(conversion);
                 outputWriter.newLine();
             }
         }
+    }
+
+    private static String getConversionString(Term initial, Term goal, Term convergence, Map<Term,Term> leftSteps, Map<Term,Term> rightSteps) {
+        List<Term> revSequence = new ArrayList<>();
+
+        Term temp =  null;
+        // Trace back the steps until we reach the start term
+        revSequence.add(convergence);
+        while (!(initial.equals(temp))) {
+            if (temp == null) {
+                if (!leftSteps.containsKey(convergence)) {
+                    break;
+                }
+                temp = leftSteps.get(convergence);
+            } else {
+                temp = leftSteps.get(temp);
+            }
+            revSequence.add(temp);
+        }
+
+        List<Term> sequence = new ArrayList<>();
+        // Reverse sequence
+        for (int i = revSequence.size() - 1; i >= 0; i--) {
+            sequence.add(revSequence.get(i));
+        }
+
+        temp =  null;
+        // Trace back the steps until we reach the start term
+        while (!(goal.equals(temp))) {
+            if (temp == null) {
+                if (!rightSteps.containsKey(convergence)) {
+                    break;
+                }
+                temp = rightSteps.get(convergence);
+            } else {
+                temp = rightSteps.get(temp);
+            }
+            sequence.add(temp);
+        }
+
+        StringBuilder stepsString = new StringBuilder();
+        stepsString.append("Conversion: ");
+        for (int i = 0; i < sequence.size(); i++) {
+            stepsString.append(sequence.get(i));
+
+            if (i < sequence.size() - 1) {
+                stepsString.append(" -> ");
+            }
+        }
+
+        return stepsString.toString();
+    }
+
+    /*
+     * BFS on terms with allEquations
+     *
+     * Rewrite all terms and subterms in terms using the equations in allEquations
+     *
+     * Returns a set of terms created as a result of this procedure
+     */
+    private static Set<Term> rewriteAll(Set<Term> terms, Set<Equation> allEquations, Map<Term, Term> steps) {
+        Set<Term> toAdd = new HashSet<>();
+
+        for (Term term : terms) {
+            for (Equation eq : allEquations) {
+                for (Term subterm : term.getAllSubTerms()) {
+                    // Try left
+                    Map<Variable, Term> sub = eq.getLeft().getSubstitution(subterm, new HashMap<>());
+
+                    if (sub != null) {
+                        Term rewriteGoal = eq.getRight();
+                        toAdd.addAll(rewriteTerm(steps, term, subterm, sub, rewriteGoal));
+                    }
+
+                    // Try right
+                    Map<Variable, Term> sub2 = eq.getRight().getSubstitution(subterm, new HashMap<>());
+
+                    if (sub2 != null) {
+                        Term newt = eq.getLeft();
+                        toAdd.addAll(rewriteTerm(steps, term, subterm, sub2, newt));
+                    }
+                }
+            }
+        }
+
+        return toAdd;
+    }
+
+    /*
+     * Substitutes all variables in newt by the term sub.variable
+     *
+     * term: whole term
+     * subterm: a subterm of term that can be rewritten to subtermSubstitute
+     * sub: the substitutions required to do this
+     * newTerm: the term obtained
+     */
+    private static Set<Term> rewriteTerm(Map<Term, Term> steps, Term term, Term subterm, Map<Variable, Term> sub, Term subtermSubstitute) {
+        Set<Term> toAdd = new HashSet<>();
+
+        /*
+         * subterm can be rewritten to subtermSubstitute using the substitutions in sub
+         * So perform all these substitutions on subtermSubstitute
+         */
+        for (Map.Entry<Variable, Term> entry : sub.entrySet()) {
+            subtermSubstitute = subtermSubstitute.substitute(entry.getKey(), entry.getValue());
+        }
+
+        // Create the new term by substituting subterm in term by subtermSubstitute
+        Term newTerm = term.substitute(subterm, subtermSubstitute);
+        toAdd.add(newTerm);
+
+        // Add this step to steps
+        if (!steps.containsKey(newTerm)) {
+            steps.put(newTerm, term);
+        }
+
+        return toAdd;
     }
 
     private static Term checkConvergence(Set<Term> left, Set<Term> right) {

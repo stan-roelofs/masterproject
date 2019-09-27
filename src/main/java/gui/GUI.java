22b0884aafaf21386c8679625e3e5bc6bf890389
Stan Roelofs <stan_roelofs@live.nl>
2019-09-27 11:15:18 +0200

    Windows look

--- src/main/java/gui/GUI.java
+++ src/main/java/gui/GUI.java
@@ -14,6 +14,17 @@ class GUI extends JFrame {
 
     GUI() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
 
         setupMenuBar();
 
